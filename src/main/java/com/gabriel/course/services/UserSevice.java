426f60a640d6035932dcd5649943ab3ed8cdcabb
Gabriel Oliveira <gabriel.oliveira184@fatec.sp.gov.br>
2021-12-07 22:41:13 -0300

    Service layer, component registration

---
+++ src/main/java/com/gabriel/course/services/UserSevice.java
@@ -0,0 +1,28 @@
+package com.gabriel.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gabriel.course.entities.User;
+import com.gabriel.course.repositories.UserRepository;
+
+
+@Service
+public class UserSevice {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> user = repository.findById(id);
+		return user.get();
+	}
+	
+}
