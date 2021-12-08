1df507486a5571eacaa734d8e194499c58dec7c3
Gabriel Oliveira <gabriel.oliveira184@fatec.sp.gov.br>
2021-12-07 22:03:42 -0300

    H2 database, test profile, JPA

--- src/main/java/com/gabriel/course/entities/User.java
+++ src/main/java/com/gabriel/course/entities/User.java
@@ -2,9 +2,19 @@ package com.gabriel.course.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "USER")
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
@@ -86,5 +96,13 @@ public class User implements Serializable{
 			return false;
 		return true;
 	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", email=" + email + ", phone=" + phone + ", password=" + password
+				+ "]";
+	}
+	
+	
 	
 }
