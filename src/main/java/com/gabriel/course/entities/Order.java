988cbc665a356fd4fca23dad70bb3f10cee0fe99
Gabriel Oliveira <gabriel.oliveira184@fatec.sp.gov.br>
2021-12-10 21:27:17 -0300

    OrderStatus Enum

--- src/main/java/com/gabriel/course/entities/Order.java
+++ src/main/java/com/gabriel/course/entities/Order.java
@@ -12,33 +12,37 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
+import com.gabriel.course.entities.enums.OrderStatus;
 
 @Entity
 @Table(name = "ORDERS")
-public class Order implements Serializable{
+public class Order implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
-	
+
+	private Integer orderStatus;
+
 	@ManyToOne
 	@JoinColumn(name = "ord_usr_id")
 	private User client;
-	
-	public Order() {}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order() {
+	}
+
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
-	
+
 	public Long getId() {
 		return id;
 	}
@@ -53,6 +57,17 @@ public class Order implements Serializable{
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null)
+			this.orderStatus = orderStatus.getCode();
+		else
+			throw new IllegalArgumentException("OrderStatus cannot be null.");
 	}
 
 	public User getClient() {
@@ -92,5 +107,5 @@ public class Order implements Serializable{
 	public String toString() {
 		return "Order [id=" + id + ", moment=" + moment + ", client=" + client + "]";
 	}
-	
+
 }
