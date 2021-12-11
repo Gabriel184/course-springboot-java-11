070f9c9ffe93a68ba035aabc0a12d04925923c7f
Gabriel Oliveira <gabriel.oliveira184@fatec.sp.gov.br>
2021-12-10 22:38:20 -0300

    Category

--- src/main/java/com/gabriel/course/resources/CategoryResource.java
+++ src/main/java/com/gabriel/course/resources/CategoryResource.java
@@ -9,26 +9,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.gabriel.course.entities.Order;
-import com.gabriel.course.services.OrderSevice;
+import com.gabriel.course.entities.Category;
+import com.gabriel.course.services.CategorySevice;
 
 @RestController
-@RequestMapping(value = "/orders")
-public class OrderResource {
+@RequestMapping(value = "/categories")
+public class CategoryResource {
 	
 	@Autowired
-	private OrderSevice orderService; 
+	private CategorySevice categorySevice; 
 	
 	@GetMapping
-	public ResponseEntity<List<Order>> findAll(){
-		List<Order> list = orderService.findAll();
+	public ResponseEntity<List<Category>> findAll(){
+		List<Category> list = categorySevice.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Order> findById(@PathVariable Long id){
-		Order o = orderService.findById(id);
-		return ResponseEntity.ok().body(o);
+	public ResponseEntity<Category> findById(@PathVariable Long id){
+		Category c = categorySevice.findById(id);
+		return ResponseEntity.ok().body(c);
 	}
 	
 }
---
+++ src/main/java/com/gabriel/course/services/CategorySevice.java
@@ -0,0 +1,28 @@
+package com.gabriel.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gabriel.course.entities.Category;
+import com.gabriel.course.repositories.CategoryRepository;
+
+
+@Service
+public class CategorySevice {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> c = repository.findById(id);
+		return c.get();
+	}
+	
+}
