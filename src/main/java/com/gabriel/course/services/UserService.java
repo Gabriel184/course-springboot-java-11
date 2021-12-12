0ce97c353c0ecd5a569f85752b963dfeea8eb579
Gabriel Oliveira <gabriel.oliveira184@fatec.sp.gov.br>
2021-12-12 19:09:03 -0300

    User delete

--- src/main/java/com/gabriel/course/services/UserService.java
+++ src/main/java/com/gabriel/course/services/UserService.java
@@ -9,23 +9,26 @@ import org.springframework.stereotype.Service;
 import com.gabriel.course.entities.User;
 import com.gabriel.course.repositories.UserRepository;
 
-
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repository;
-	
+
 	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> user = repository.findById(id);
 		return user.get();
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
+	}
+
+	public void deleteById(Long id) {
+		repository.deleteById(id);
 	}
 }
